82cebe9558f06865633958fb9333efe73101bb13
onishi taisei <taisei.developer@gmail.com>
2023-09-03 23:43:31 +0900

    🚀 Add Notification

--- lib/features/notification/notification_page.dart
+++ lib/features/notification/notification_page.dart
@@ -1,7 +1,6 @@
+import 'package:day_night_time_picker/day_night_time_picker.dart';
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:meditation_life/shared/res/color.dart';
-import 'package:meditation_life/utils/vibration_util.dart';
 
 class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
@@ -12,8 +11,6 @@ class NotificationPage extends StatefulWidget {
 
 class NotificationPageState extends State<NotificationPage> {
   bool _pushNotifications = true;
-  final bool _emailNotifications = true;
-  final bool _inAppNotifications = true;
 
   @override
   Widget build(BuildContext context) {
@@ -38,20 +35,22 @@ class NotificationPageState extends State<NotificationPage> {
       ),
       body: Column(
         children: [
-          Consumer(
-            builder: (context, ref, child) {
-              return _SettingsSwitchTile(
-                title: 'プッシュ通知',
-                value: _pushNotifications,
-                onChanged: (value) {
-                  ref
-                      .read(vibrationProvider)
-                      .impact(HapticFeedbackType.mediumImpact);
-                  setState(() {
-                    _pushNotifications = value;
-                  });
-                },
-              );
+          _SettingsSwitchTile(
+            title: '通知オン/オフ',
+            value: _pushNotifications,
+            onChanged: (value) {
+              setState(() {
+                _pushNotifications = value;
+              });
+            },
+          ),
+          _NotificationDateTile(
+            title: 'プッシュ通知',
+            value: _pushNotifications,
+            onChanged: (value) {
+              setState(() {
+                _pushNotifications = value;
+              });
             },
           )
         ],
@@ -95,6 +94,61 @@ class _SettingsSwitchTile extends StatelessWidget {
           value: value,
           onChanged: onChanged,
           activeColor: AppColor.secondary,
+        ),
+      ),
+    );
+  }
+}
+
+class _NotificationDateTile extends StatelessWidget {
+  final String title;
+  final bool value;
+  final Function(bool) onChanged;
+
+  const _NotificationDateTile({
+    required this.title,
+    required this.value,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey[200]!,
+          ),
+        ),
+      ),
+      child: ListTile(
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        title: const Text(
+          "通知時間",
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        trailing: GestureDetector(
+          onTap: () => Navigator.of(context).push(
+            showPicker(
+              context: context,
+              value: Time(hour: 10, minute: 30),
+              sunrise: const TimeOfDay(hour: 6, minute: 0),
+              sunset: const TimeOfDay(hour: 18, minute: 0),
+              duskSpanInMinutes: 120, // optional
+              onChange: (time) {},
+            ),
+          ),
+          child: const Text(
+            "00：00",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:isar/isar.dart';
@@ -20,6 +23,8 @@ import 'package:meditation_life/utils/shared_preference_util.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:timezone/data/latest.dart' as timezone;
+import 'package:timezone/timezone.dart' as tz;
 
 final meditationRepositoryProvider = Provider<MeditationRepository>(
   (_) => throw UnimplementedError(),
@@ -67,7 +72,13 @@ Future<void> main() async {
     ],
   );
 
-  await setUp(container);
+  timezone.initializeTimeZones();
+
+  await _requestNotificationPermission();
+
+  await _setUp(container);
+
+  await _scheduleDaily8AMNotification();
 
   runApp(
     UncontrolledProviderScope(
@@ -80,11 +91,69 @@ Future<void> main() async {
   );
 }
 
-Future<void> setUp(ProviderContainer container) async {
+Future<void> _setUp(ProviderContainer container) async {
   final authRepository = container.read(authRepositoryProvider);
   final userRepository = container.read(userRepositoryProvider);
   if (authRepository.authUser == null) {
     await authRepository.signInWithAnonymously();
     await userRepository.createUser();
   }
+}
+
+Future<void> _requestNotificationPermission() async {
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  if (Platform.isIOS) {
+    await flutterLocalNotificationsPlugin
+        .resolvePlatformSpecificImplementation<
+            IOSFlutterLocalNotificationsPlugin>()
+        ?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+  } else if (Platform.isAndroid) {
+    final androidImplementation =
+        flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
+            AndroidFlutterLocalNotificationsPlugin>();
+    await androidImplementation?.requestPermission();
+  }
+}
+
+Future<void> _scheduleDaily8AMNotification() async {
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin.zonedSchedule(
+    312,
+    '瞑想ライフ',
+    '本日の瞑想を行いましょう！',
+    _fiveSecondsLater(),
+    const NotificationDetails(
+      android: AndroidNotificationDetails(
+        '',
+        '',
+        channelDescription: '',
+      ),
+      iOS: DarwinNotificationDetails(badgeNumber: 1),
+    ),
+    uiLocalNotificationDateInterpretation:
+        UILocalNotificationDateInterpretation.absoluteTime,
+  );
+}
+
+// 1回目に通知を飛ばす時間の作成
+tz.TZDateTime _nextInstanceOf8AM() {
+  final now = tz.TZDateTime.now(tz.local);
+  print(now);
+  tz.TZDateTime scheduledDate =
+      tz.TZDateTime(tz.local, now.year, now.month, now.day, 13, 54);
+  if (scheduledDate.isBefore(now)) {
+    scheduledDate = scheduledDate.add(const Duration(days: 1));
+  }
+  return scheduledDate;
+}
+
+// デバッグ用（3秒後に通知）
+tz.TZDateTime _fiveSecondsLater() {
+  var later = tz.TZDateTime.now(tz.local);
+  later = later.add(const Duration(seconds: 5));
+  return later;
 }

6e1d3dc3b914faacb07a6502e05840f756e6f452
onishi taisei <taisei.developer@gmail.com>
2023-10-21 23:12:43 +0900

    revert "🚜 refactor: RiverpodLoggerに関連するソースコードを修正"
    This reverts commit b543d59d41cc5abc70dbe69f29015b68647c11ac.

--- lib/utils/riverpod_logger.dart
+++ lib/utils/riverpod_logger.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class RiverpodLogger extends ProviderObserver {
-  const RiverpodLogger._();
-
   @override
   void didAddProvider(
     ProviderBase<Object?> provider,
